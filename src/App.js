579ab5d62fa22c9db8a70528db9b7672cf1d8d1b
Dennis Peters <dennis95peters@gmail.com>
2021-08-27 10:33:55 +0300

    Toggle the music library into and out of view

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "./styles/app.scss";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import Library from "./components/Library";
+import Nav from "./components/Nav";
 
 //Add Util
 import data from "./util"
@@ -24,6 +25,7 @@ function App() {
     currentTime: 0,
     duration: 0,
   });
+  const [libraryStatus, setLibraryStatus] = useState(false);
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
@@ -31,6 +33,7 @@ function App() {
   };
   return (
     <div className="App">
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song 
         currentSong={currentSong} 
       />
@@ -48,6 +51,7 @@ function App() {
         setCurrentSong={setCurrentSong}
         isPlaying={isPlaying}
         setSongs={setSongs}
+        libraryStatus={libraryStatus}
       />
 
       <audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref={audioRef} src={ currentSong.audio }></audio>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMusic } from '@fortawesome/free-solid-svg-icons';
 
-const Nav = () => {
+const Nav = ({setLibraryStatus, libraryStatus}) => {
     return(
         <nav>
             <h1>Music</h1>
-            <button>
+            <button onClick={() => setLibraryStatus(!libraryStatus)}>
                 Library
                 <FontAwesomeIcon icon ={faMusic} />
             </button>
         </nav>
     )
 }
+
+export default Nav;
